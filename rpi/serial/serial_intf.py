a2628a0910c442f94ec11c0736ed3900fafd02bc
greenbreakfast <lazardemin@gmail.com>
2014-02-22 18:14:48 -0500

    removed extra print statements

--- rpi/serial/serial_intf.py
+++ rpi/serial/serial_intf.py
@@ -17,12 +17,12 @@ port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
 numArguments = len(sys.argv)
 
 if numArguments == 0:
-	print "Invalid arguments\n"
+	#print "Invalid arguments\n"
 	sys.exit(1)
 else:
 	action = str(sys.argv[1])
-	print "Performing action '%s'" % action
-	print "Have '%d' arguments" % numArguments
+	#print "Performing action '%s'" % action
+	#print "Have '%d' arguments" % numArguments
 
 numBytesWritten = 0
 # receive data from arduino
@@ -32,21 +32,21 @@ if numArguments == 2:
 		sys.exit(1)
 	else:
 		numBytesWritten += port.write("<1>");
-		print "Number of bytes written: '%d'" % numBytesWritten
+		#print "Number of bytes written: '%d'" % numBytesWritten
 		recvStr = readline(port)
 		print (recvStr)
 
 # send a string of data to arduino	
 elif numArguments == 3:
 	if (action!="send"):
-		print "Invalid command, expecting 'send'\n"
+		#print "Invalid command, expecting 'send'\n"
 		sys.exit(1)
 	else:
 		command = str(sys.argv[2])
 		sendStr	= "<2><%s>" % command
-		print "Sending command '%s'\n" % sendStr
+		#print "Sending command '%s'\n" % sendStr
 		numBytesWritten += port.write(sendStr)
-		print "Number of bytes written: '%d'" % numBytesWritten
+		#print "Number of bytes written: '%d'" % numBytesWritten
 
 else:
 	sys.exit(1)
