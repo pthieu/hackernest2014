8cbe70d8b5d89dfd84abf5603c03bcfa7d4f9275
calvinngan <calvinthomasonngan@gmail.com>
2014-02-22 17:39:34 -0500

    updated record_ir to search and replace

--- rpi/bash/record_ir.sh
+++ rpi/bash/record_ir.sh
@@ -3,3 +3,40 @@
 # Output: write to table in “IR Cmd File”
 # Process: Execute “Communication Processor” and pool for return
 
+#!/bin/bash -x
+
+# 1 arg
+function chk_arg
+{
+    if [ -z "$1" ]; then
+        echo missing argument
+        exit
+    fi
+}
+
+chk_arg $1
+
+IR_NAME=$1
+IR_FILE="../config/ir_cmd.config"
+IR_CMD="0xFF"
+FIND_NAME=0
+echo $IR_NAME
+# call communication processor
+# record result to file
+if [ -e "$IR_FILE" ]; 
+then
+    # check if there exist IR_NAME already
+    FIND_NAME=`grep -w $IR_NAME $IR_FILE -c`
+    if [ "$FIND_NAME" -gt "0" ]; 
+    then
+        echo $IR_NAME exist
+        # replace $IR_NAME line
+        sed -i "s/^$IR_NAME.*/$IR_NAME $IR_CMD/" "$IR_FILE"
+    else
+        echo "$IR_NAME $IR_CMD" >> $IR_FILE
+    fi
+else
+    # create the file and write the first command
+    echo creating file
+    echo "$IR_NAME $IR_CMD" >> $IR_FILE
+fi
