4bb3bd15e8891a1063660433e16ec5b1d6ecb50c
greenbreakfast <lazardemin@gmail.com>
2014-02-22 22:37:45 -0500

    added ir sketch and local copy of serial intf script

---
+++ rpi/serial/serial_intf_local.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import serial
+import sys
+
+def readline(port):
+    rv = ""
+    while True:
+        ch = port.read()
+        rv += ch
+        if ch=='\r' or ch=='':
+            return rv
+
+#port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
+port = serial.Serial("/dev/tty.usbmodemfa131", baudrate=9600, timeout=3.0)
+
+numArguments = len(sys.argv)
+
+if numArguments <= 1:
+	print "Invalid arguments\n"
+	sys.exit(1)
+else:
+	action = str(sys.argv[1])
+	#print "Performing action '%s'" % action
+	#print "Have '%d' arguments" % numArguments
+
+numBytesWritten = 0
+# receive data from arduino
+if numArguments == 2:
+	if (action!="record"):
+		print "Invalid command, expecting 'record'\n"
+		sys.exit(1)
+	else:
+		numBytesWritten += port.write("<1>");
+		print "Number of bytes written: '%d'" % numBytesWritten
+		recvStr = readline(port)
+		print "Received: '%s'" % recvStr
+
+# send a string of data to arduino	
+elif numArguments == 3:
+	if (action!="send"):
+		#print "Invalid command, expecting 'send'\n"
+		sys.exit(1)
+	else:
+		command = str(sys.argv[2])
+		sendStr	= "<2><%s>" % command
+		print "Sending command '%s'\n" % sendStr
+		numBytesWritten += port.write(sendStr)
+		print "Number of bytes written: '%d'" % numBytesWritten
+
+else:
+	sys.exit(1)
