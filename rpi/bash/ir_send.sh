239e55cb88808347e7a861f1d8fee0cfc58db35c
calvinngan <calvinthomasonngan@gmail.com>
2014-02-23 03:51:06 -0500

    renamed record_ir to ir_record updated file system , upgrated scripts to support the new file system

--- rpi/bash/ir_send.sh
+++ rpi/bash/ir_send.sh
@@ -1,10 +1,12 @@
 # Run IR Cmd/Macro:
-# Input: ARG[0]: IR code(s) file name,
-#           ARG[1]: name of cmd ID or macro id
+# Input: ARG[0]: name of cmd ID or macro id
 # Output: N/A
 # Process: 
 # read file, find name, extract IR code
 # Execute “Communication processor” for each IR code (non blocking)
+# macro name;; sub macro names;; ir cmd(s);; number of time used
+# ( sub macro names omitted if macro is not composed of macros. ie the first ir
+# cmd)
 
 #!/bin/bash -x
 
@@ -20,9 +22,8 @@ function chk_arg
 runpath="`pwd`/rpi"
 
 chk_arg $1
-chk_arg $2
-FILE="$runpath/config/$1"
-NAME=$2
+FILE="$runpath/config/macro.config"
+NAME=$1
 COM_PROC="$runpath/serial/serial_intf.py send"
 
 # check if file exist
@@ -31,13 +32,20 @@ then
     echo file found
     # look for name in the file
     echo $NAME
-    CMD=`grep -w "$NAME:" $FILE | sed s/"$NAME:"//`
+    MACRO_ENTRY=`grep -w "^$NAME::" $FILE`
+    echo $MACRO_ENTRY
+    CMD=`echo $MACRO_ENTRY | eval "sed -n -e 's/^"$NAME"::.*::\(.*\)::.*$/\1/p'"`
     chk_arg $CMD
+    COUNTER=`echo $MACRO_ENTRY | eval "sed -n -e 's/^"$NAME"::.*::.*::\(.*\)$/\1/p'"`
+    chk_arg $COUNTER
     echo $CMD
+    COUNTER=$(($COUNTER+1))
+    eval "sed -i -e 's/\(^"$NAME"::.*::.*::\).*$/\1 "$COUNTER"/' $FILE"
+    echo $COUNTER
     
     for i in $CMD;
     do
-        echo Send $i
+        #echo Send $i
         python $COM_PROC $CMD
     done
 else
